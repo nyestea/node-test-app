3bf9d613c2362019bf4af0aaa75930ab59c430f3
Attila Nyeste <attilany@amazon.com>
2025-05-16 15:30:19 +0200

    adding name variable

--- index.mjs
+++ index.mjs
@@ -1,8 +1,10 @@
 export const handler = async (event) => {
   // TODO implement
+  var nameParam = event.queryStringParameters.name;
+
   const response = {
     statusCode: 200,
-    body: JSON.stringify('Hello Attila from Lambda!'),
+    body: JSON.stringify('Hello' + nameParam + ' from Lambda!')
   };
   return response;
 };
